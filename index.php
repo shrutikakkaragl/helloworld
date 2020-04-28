6d677ba9585ca146ebf0ed7054b7b48c35a66d3e
shrutikakkaragl <shruti.kakkar@adglobal360.com>
2020-04-28 17:37:17 +0530

    first push

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+date_default_timezone_set('Asia/Kolkata');
+//echo date("Y-m-d H:i:s");exit;
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+ 
+//error_reporting("-1");
+ini_set('display_errors', 0); 
+
+//error_reporting(E_ALL);
+//chdir(dirname(__DIR__));
+// die('maintenance downtime');
+//error_reporting(E_ALL & ~E_NOTICE);
+// dirname(__DIR__).'\adv8';
+chdir(dirname(__DIR__).'\agldashboard-maruti');
+
+//chdir(dirname(__DIR__).'\\');
+// Setup autoloading
+require 'init_autoloader.php';
+// Run the application!
+//echo "f";exit;
+
+Zend\Mvc\Application::init(require 'config/application.config.php')->run();
+
+?>
